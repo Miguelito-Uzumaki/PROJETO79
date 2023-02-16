09f9f95e8e84c6de7fe77f93c9d20cb0cd85635c
Miguelito-Uzumaki <119423772+Miguelito-Uzumaki@users.noreply.github.com>
2023-02-16 16:07:04 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+menuListArray = ["Pizza Vegetariana",
+                  "Pizza de Frango",
+                  "Pizza Portuguesa",
+                  "Pizza Quatro Queijos",
+                  "Pizza de Calabresa",
+                  "Pizza Estravaganza"];
+var imgtags='<img id="im1" src="imagens/pizzaImg.png"/>'
+function getMenu(){
+var htmldata="";
+    for(var i=0;i<menuListArray.length;i++){
+        htmldata=htmldata+ menuListArray[i] + '<br>'
+    }
+    document.getElementById("displayMenu").innerHTML = htmldata;
+}
+
+function addItem(){
+var htmldata;
+var item=document.getElementById("addItem").value;
+menuListArray.sort();
+htmldata=""
+for(var i=0;i<menuListArray.length;i++){
+    htmldata+htmldata+imgtags+ menuListArray[i]+ '<br>';
+}
+document.getElementById("displayAddedMenu").innerHTML = htmldata;
+}
+
+function addTop(){
+var item=document.getElementById("addItem").value;
+menuListArray.push(item);
+addItem();
+}
